0d2549b590aa18035a78f2b5fa4ba4b278982730
Laurence279 <lnunn279@gmail.com>
2021-12-29 21:01:23 +0000

    Add request handlers

--- routes/trees.js
+++ routes/trees.js
@@ -2,6 +2,7 @@
 
 
 import express from "express";
+import trees from "../exampleTreeData";
 const router = express.Router();
 
 
@@ -16,11 +17,56 @@ const router = express.Router();
 // GET all trees
 router.get("/", async (req, res) => {
 
+    //Query Handler???
+
+    //Function to get all trees
     res.json({
         success: true,
-        message: `IT WORKS`
+        message: `Retrieved all trees`
     });
 });
+
+router.get("/:id", async (req, res) => {
+    //Function to get tree by ID
+    res.json({
+        success: true,
+        message: `Retrieved tree with id ${req.params.id}`
+    })
+})
+
+router.post("/", async (req, res) => {
+    // Function to create new tree
+    res.json({
+        success: trees,
+        message: "Successfully created new tree.."
+    })
+})
+
+router.put("/:id", async (req, res) => {
+    // function to replace tree
+    res.json({
+        success: true,
+        message: `Replaced tree at id ${req.params.id}`
+    })
+})
+
+router.patch("/:id", async (req, res) => {
+    const query = req.query;
+    // function to update tree
+    res.json({
+        success: true,
+        message: `Updated tree at id ${req.params.id}`
+    })
+})
+
+router.delete("/:id", async (req, res) => {
+    //function to delete tree by id
+    res.json({
+        success: true,
+        message: `Deleted tree at id ${req.params.id}`
+    })
+})
+
 
 
 
