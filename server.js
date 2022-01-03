52a237f2faa3df99117a643d4280eb0da659ff9e
Laurence279 <lnunn279@gmail.com>
2022-01-03 01:01:35 +0000

    Heroku build 1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 // set up the server PORT
 app.listen(PORT, function () {
     console.log(`Server listening on port ${PORT}`)
